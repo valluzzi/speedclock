f74ca6cf079f8b8d8e2c3fa360f3e29f1b915a1e
valerioluzzi <Adamondra01>
2021-06-20 10:25:22 +0200

    Wemos new version

---
+++ Wemos D1/wemos01/functions.h
@@ -0,0 +1,164 @@
+//-------------------------------------------------------------------------------
+// Licence:
+// Copyright (c) 2019-2021 Luzzi Valerio 
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+// Name:         functions.h
+// Purpose:      A clock for speed climb
+//
+// Author:      Luzzi Valerio
+//
+// Created:     19/06/2021
+//-------------------------------------------------------------------------------
+#include <ESP8266WiFi.h> //ESP8266
+#include <WiFiUdp.h>
+
+#define ID 0  //foot-board id 0,1,2,...
+#define localPort 10999      // local port to listen on
+#define remotePort 11000     // remote display port
+
+#define DEBUG 1
+#define START_BUTTON 2   //Wemos D4 pin
+#define STOP_BUTTON 0    //Wemos D3 pin
+
+
+unsigned long ts;  //timestamp of start
+unsigned long te;  //timestamp of end
+
+
+
+WiFiUDP Udp;
+const IPAddress broadcastIp(255,255,255,255);
+
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+/**
+ * WiFISetup - connect to a WiFi Networka and 
+ */
+void WiFiSetup(){
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID_1, SECRET_PASS_1);
+
+  // check for the presence of the shield:
+  while (WiFi.status() == WL_NO_SHIELD) {
+      Serial.println("WiFi shield not present");
+      delay(1000);
+  }
+
+  // attempt to connect to WiFi network:
+  while ( WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID_1);
+    // wait 2 seconds for connection:
+    Serial.println("------------------------------------------------");
+    delay(2000);
+  }
+  
+  Udp.begin(localPort);
+  
+  Serial.println("Connected to wifi");
+  printWiFiStatus();
+
+}
+
+
+
+
+char packet[32]; //buffer to broadcast  
+unsigned long mills_in_this_state=0;
+unsigned long LOOP_TIME;
+
+enum state {
+  BEGIN,
+  IDLE,
+  PREARMED,
+  ARMED,
+  RUNNING,
+  STOP
+};
+
+state STATE;
+
+/**
+ * broadcast - an udp packet 
+ */
+void broadcast(char* text){
+
+    Udp.beginPacket(broadcastIp, remotePort);
+    Udp.write(text);
+    Udp.endPacket();
+    if (DEBUG)
+      Serial.println(text);
+}
+
+/**
+ * broadcast_my_state
+ */
+void broadcast_my_state(){
+
+  switch (STATE) {
+
+      case BEGIN:
+        sprintf(packet,"%d:BEGIN:%lu",ID,0);
+        break;
+      case IDLE:
+        sprintf(packet,"%d:IDLE:%lu",ID,0);
+        break;
+      case PREARMED:
+        sprintf(packet,"%d:PREARMED:%lu",ID,0);
+        break;
+      case ARMED:
+        sprintf(packet,"%d:ARMED:%lu",ID,0);
+        break;
+      case RUNNING:
+        sprintf(packet,"%d:RUNNING:%lu",ID,millis()-ts);
+        break;
+      case STOP:
+        sprintf(packet,"%d:STOP:%lu",ID,te-ts);
+        break;
+      default:
+        sprintf(packet,"%d:IDLE:%lu",ID,0);
+        
+    }//end switch
+    
+    broadcast(packet);
+}//end broadcast my state
+
+/**
+ * changeStateTo - change state and broadcast it
+ */
+void changeStateTo(state s){
+  
+    STATE = s;
+    mills_in_this_state = 0;
+    broadcast_my_state();
+}
---
+++ Wemos D1/wemos01/wemos01.ino
@@ -0,0 +1,108 @@
+//-------------------------------------------------------------------------------
+// Licence:
+// Copyright (c) 2019-2021 Luzzi Valerio 
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+// Name:         wemos01.ino
+// Purpose:      A clock for speed climb
+//
+// Author:      Luzzi Valerio
+//
+// Created:     19/06/2021
+//-------------------------------------------------------------------------------
+#include "secrets.h" 
+#include "functions.h" 
+
+//-------------------------------------------------------------------------------
+//
+//
+//    +-------+     +------+       +----------+       +---------+       +---------+       +---------+
+//    |BEGIN  |---->| IDLE |------>| PREARMED |------>| ARMED   |------>| RUNNING |------>|   STOP  |
+//    +-------+     +------+       +----------+       +---------+       +---------+       +---------+    
+//                       ^              |                   |                 |                 |                          
+//                       |              |                   |                 |                 |                         
+//                       +--------------+<------------------+<----------------+<----------------+
+//
+//
+
+
+bool isFootBoardPressed  = false;
+bool isStopButtonPressed = false;
+
+void setup() {
+
+  // initialize the button pin as a input:
+  pinMode(START_BUTTON, INPUT_PULLUP);
+  pinMode(STOP_BUTTON,  INPUT_PULLUP);
+  
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+
+  WiFiSetup();
+  
+
+  changeStateTo(BEGIN);
+
+  changeStateTo(IDLE);
+
+}
+
+void loop() {
+
+      LOOP_TIME = millis();
+      isFootBoardPressed  = (digitalRead(START_BUTTON)==LOW);
+      isStopButtonPressed = (digitalRead(STOP_BUTTON) ==LOW);
+
+      //If footbar is pressed goto PREARMED
+      if (STATE==IDLE && isFootBoardPressed){
+          changeStateTo(PREARMED);
+      }
+      //After 2s in PREARMED we go ARMED
+      else if (STATE==PREARMED && isFootBoardPressed && mills_in_this_state>2000){
+          changeStateTo(ARMED);
+      }
+      //otherwhise return in IDLE
+      else if (STATE==PREARMED && !isFootBoardPressed){
+          changeStateTo(IDLE);
+      }
+      //if ARMED and we leave footbar goto RUNNING
+      else if (STATE==ARMED && !isFootBoardPressed){
+          ts = millis();
+          changeStateTo(RUNNING);
+      }
+      //a pression on footboard while RUNNING of 2s cause return in IDLE
+      else if (STATE==RUNNING && isFootBoardPressed && mills_in_this_state>2000){
+          Serial.println("Returning to IDLE..");
+          changeStateTo(IDLE);
+      }
+      //On STOP button goto STOP state
+      else if (STATE==RUNNING && isStopButtonPressed){
+          te = millis();
+          Serial.println("!!!STOP STOP STOP!!!");
+          changeStateTo(STOP);
+      }
+      else if (STATE==STOP && isFootBoardPressed){
+          changeStateTo(IDLE);
+      }
+      //Every 500ms broadcast the state 
+      else if (mills_in_this_state%500==0){
+          broadcast_my_state();
+      }
+
+      delay(1);
+
+      LOOP_TIME = millis()-LOOP_TIME;
+      mills_in_this_state+=LOOP_TIME;
+}//loop
