2803b284437a07126ba7fcfec3c41fd07a413311
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2021-06-21 08:56:53 +0200

    SpeedClimb app

---
+++ SpeedClimb/app/src/main/java/com/gmail/valluzzi/speedclimb/Utils.java
@@ -0,0 +1,10 @@
+package com.gmail.valluzzi.speedclimb;
+
+
+public class Utils {
+
+    public static void sleep(int mills)
+    {
+        try{Thread.sleep(mills);}catch(Exception ex){}
+    }
+}//end class
