8ac5d3d814915b9c3cb8a6db3878c734e971d497
Andrea Pagliarani <andrea.pagliarani6@studio.unibo.it>
2021-06-28 23:52:59 +0200

    multiple position management

---
+++ SpeedClimb/app/src/main/java/com/gmail/valluzzi/speedclimb/model/Positions.java
@@ -0,0 +1,11 @@
+package com.gmail.valluzzi.speedclimb.model;
+
+public class Position {
+    private final int ID;
+
+    public Position(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {return this.ID;}
+}
